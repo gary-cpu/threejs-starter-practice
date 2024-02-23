a2e83e2404b318ea013acaa419503f71e0392362
gary-cpu <59800574+gary-cpu@users.noreply.github.com>
2024-02-23 16:35:37 -0500

    Update main.js.

--- main.js
+++ main.js
@@ -5,9 +5,6 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer(); 
  
 function init() {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
