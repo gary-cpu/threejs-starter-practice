4412618dbcda6c8ab2286dfccf907a5bc8708fef
gary-cpu <59800574+gary-cpu@users.noreply.github.com>
2024-02-24 16:29:30 -0500

    z

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 let model;
+let angle = 0;
 
 function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
